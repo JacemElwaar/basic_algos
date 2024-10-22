7ed61566a6f8793dd7d2bba2a94811cc9547e9fd
jacem elwaar <elwaarjacem@gmail.com>
2024-10-22 15:41:00 +0200

    start AI starter based on search algos

---
+++ search_algos/ai_basics_mase.py
@@ -0,0 +1,27 @@
+class Maze(): 
+    def __init__(self):
+        pass
+    def create(self, path= None):
+        if path is None:
+            return True
+        return path  
+
+class StackFrontier(): 
+    def __init__(self):
+        pass
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+
+class QueueFrontier():
+    def __init__(self):
+        pass
+    def add(self):
+        pass
+
+    def remove(self):
+        pass 
+
